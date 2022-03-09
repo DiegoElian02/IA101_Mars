39c5687447a1e77228e0bb546a4cba6044c735f3
DiegoElian02 <A00829925@itesm.mx>
2022-03-08 18:59:57 -0600

    estética
    se eliminaron comentarios innecesarios y redujo codigo

--- proyectoDdAI.py
+++ proyectoDdAI.py
@@ -7,11 +7,8 @@ import time
 
 inicio = time.time()
 
+
 def astar(lab, i_s, j_s, i_e, j_e, h):
-    # identifica el punto de salida
-    # (i_s, j_s) = [[(i, j) for j, cell in enumerate(row) if cell == s_value] for i, row in enumerate(lab) if s_value in row][0][0]
-    # #identifica el punto de meta
-    # (i_e, j_e) = [[(i, j) for j, cell in enumerate(row) if cell == e_value] for i, row in enumerate(lab) if e_value in row][0][0]
 
     #establece el tamaño del laberinto
     width = len(lab[0])
@@ -21,7 +18,7 @@ def astar(lab, i_s, j_s, i_e, j_e, h):
     #lambda es una función anónima, significa que 'heuristic' está en función de i y j
     heuristic = lambda i, j: math.sqrt((i_e - i)**2+(j_e - j)**2)
 
-    #obtiene el costo total (heurística + el camino recorrido)
+    #obtiene el costo total (el camino recorrido + heurística)
     comp = lambda state: state[2] + state[3]
 
     #fringe = casillas sin explorar (lista de tuplas). 
@@ -32,7 +29,6 @@ def astar(lab, i_s, j_s, i_e, j_e, h):
     #diccionario vacío, este tendrá las casillas visitadas junto con su path cost
     visited = {}
 
-    #maybe limit to prevent too long search
     while True:
 
         #obtiene el primer objeto de la lista (least cost) y lo elimina de esta
@@ -48,49 +44,47 @@ def astar(lab, i_s, j_s, i_e, j_e, h):
             print(state)
             return path
 
-        #set the cost (path is enough since the heuristic won't change) ????
+        #set the cost
         visited[(i, j)] = state[2] + state[3]
 
-        #explorar el vecino, cuatro direcciones
+        #explorar el vecino, ocho direcciones
         neighbor = list() #se agregan a la lista de vecinos
-        #cont = 0
+
         #revisa que esté dentro de la matriz y que sea diferente a la barrera
         if i > 0 and abs(lab[i-1][j] - lab[i][j]) <= h and not(lab[i-1][j] in visited): #subir
             neighbor.append((i-1, j))
-            #cont = cont + 1
+
         if i < height and abs(lab[i+1][j] - lab[i][j]) <= h and not(lab[i+1][j] in visited): #bajar
             neighbor.append((i+1, j))
-            #cont = cont + 1
+
         if j > 0 and abs(lab[i][j-1] - lab[i][j]) <= h and not(lab[i][j-1] in visited): #izq
             neighbor.append((i, j-1))
-            #cont = cont + 1
+
         if j < width and abs(lab[i][j+1] - lab[i][j]) <= h and not(lab[i][j+1] in visited): #der
             neighbor.append((i, j+1))
-            #cont = cont + 1
+
         if (i > 0 and j > 0) and abs(lab[i-1][j-1] - lab[i][j]) <= h and not(lab[i-1][j-1] in visited): #subir izq
             neighbor.append((i-1, j-1))
-            #cont = cont + 1
+
         if (i < height and j > 0) and abs(lab[i+1][j-1] - lab[i][j]) <= h and not(lab[i+1][j-1] in visited): #bajar izq
             neighbor.append((i+1, j-1))
-            #cont = cont + 1
+
         if (i > 0 and j < width) and abs(lab[i-1][j+1] - lab[i][j]) <= h and not(lab[i-1][j+1] in visited): #subir der
             neighbor.append((i-1, j+1))
-            #cont = cont + 1
+
         if (i < height and j < width) and abs(lab[i+1][j+1] - lab[i][j]) <= h and not(lab[i+1][j+1] in visited): #bajar der
             neighbor.append((i+1, j+1))
-            #cont = cont + 1
 
-        for n in neighbor: #para cada vecino
+        for n in neighbor:
             next_cost = heuristic(n[0],n[1]) #definir que el siguiente path cost será el mismo + 1 (se aleja 1 del inicio)
-            # if n in visited and visited[n] >= next_cost: #si n ya fue visitado y el costo de n es mayor o igual al siguiente, continúa
-            #     continue
             fringe.append((n, [state[0]] + state[1], state[2] + 1, next_cost)) #se agrega la coordenada nueva,
             #la lista de los ya recorridos, el costo del lugar siguiente, y el costo heurístico
         
-        print(state) #imprimir moves
+        #print(state) #imprimir moves
 
         #reorganiza la lista con base en 'comp', es decir, el costo total
         fringe.sort(key=comp)
+
 
 def printsolution(sol, lab):
     for coord in range(0, len(sol)):
@@ -100,19 +94,18 @@ def printsolution(sol, lab):
     fig = px.imshow(mars_map)
     fig.show()
 
+
 # MAIN ALGORITHM
 mars_map = np.load('mars_map.npy')
-#print(mars_map)
 
-i_start = 1771
-j_start = 383
-i_end = 690
-j_end = 160
-h = 0.25
-# sv = mars_map[i_start][j_start]
-# ev = mars_map[i_end][j_end]
+#valores de busqueda
+y_start = 1237
+x_start = 394
+y_end = 1172
+x_end = 620
+block = 0.25
 
-road = astar(mars_map, i_start, j_start, i_end, j_end, h)
+road = astar(mars_map, y_start, x_start, y_end, x_end, block)
 print(road)
 print(printsolution(road, mars_map))
 
